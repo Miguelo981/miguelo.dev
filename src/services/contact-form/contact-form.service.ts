58680bcdf5801ee89caf82b39cc9252c1f8f3021
Miguelo981 <55580212+Miguelo981@users.noreply.github.com>
2023-06-20 22:08:14 +0200

    Remove old components and services

--- src/services/contact-form/contact-form.service.ts
+++
@@ -1,8 +0,0 @@
-import type { ContactMeForm } from "../../models/forms/contact-me.model";
-import { makePostRequest } from "../request/request.service";
-
-const endPoint = "/contact-form";
-
-export async function createForm(form: ContactMeForm) {
-    return await makePostRequest(`${endPoint}/`, form);
-}
--- src/services/services/services.service.ts
+++
@@ -1,8 +0,0 @@
-import type { Service } from "../../models/service.model";
-import { makeGetRequest } from "../request/request.service";
-
-const endPoint = "/services";
-
-export async function getServices(): Promise<Service[]> {
-    return await makeGetRequest(`${endPoint}/`);
-}
